cfb7f5cb65f86cbab56f706f9a1ad247242c244d
DevlikanovRK <devlikanov@vk.com>
2025-02-01 18:54:19 +0300

    hw03 - snake patr 1

---
+++ hw03_snake/52.c
@@ -0,0 +1,13 @@
+#include <curses.h>
+#include <unistd.h>
+
+int main() {
+    initscr();
+    printw("Hello World !!!");
+    printw("\nPress any key to continue... ");
+    refresh(); // по пробуйте включить и выключить
+    sleep(3);
+    getch();
+    endwin();
+    return 0;
+}
